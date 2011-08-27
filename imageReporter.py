9f4b570f8c97f3a30db081bc25a2c76bc58b5914
Vincent Berthoux <twinside@gmail.com>
2011-08-27 11:19:25 +0200

    _MUCH_ faster test automation

--- imageReporter.py
+++ imageReporter.py
@@ -1,70 +1,45 @@
 import glob
 import subprocess
 import os
-import re
-import shutil
-
-def leftPart( rez, elems ):
-    [a,b,c] = elems
-    return [rez, b, c]
-
-def rightPart( rez, elems ):
-    [a,b,c] = elems
-    return [a, b, rez]
-
-def middlePart( rez, elems ):
-    [a,b,c] = elems
-    return [a, rez, c]
-
-searches = [ ('Left file: (.*) &nbsp;', leftPart )
-           , ('Right file: (.*) &nbsp;', rightPart)
-           , ('<img src="([^"]+)".*', middlePart) ]
-
-def doReport( filename1, filename2 ):
-    beyondCompareScriptLine = "picture-report layout:side-by-side output-to:t.html output-options:html-color "
-
-    script_file = open("test_script.txt", "w")
-    script_file.write(beyondCompareScriptLine + filename1 + " " + filename2 )
-    script_file.close()
-
-    subprocess.call(["C:\Program Files (x86)\Beyond Compare 3\BComp.com", "/closescript", "@test_script.txt"])
-
-    infile = open("t.html", "r")
-    ret = ["", "", ""]
-    for line in infile:
-        for (rexp, action) in searches:
-            rez = re.search(rexp, line)
-            if rez:
-                ret = action(rez.group(1), ret)
-    infile.close()
-    return ret
 
 def performTests():
     ofile = open("testResult.html","w")
     ofile.write("<html><head><title>Test results</title></head><body>")
 
+    beyondCompareScriptLine = "picture-report layout:side-by-side output-to:t.html output-options:html-color "
+
+    script_file = open("test_script.txt", "w")
     counter = 0
     for pngFile in glob.glob('tests/pngsuite/*.png'):
-    	bmpFile = pngFile + ".bmp"
-    	if os.path.exists(bmpFile):
-            (left, comp, right) = doReport(pngFile, bmpFile)
-            neofile = "BcImages/Diff_" + str(counter) + ".png"
-            shutil.copyfile("BcImages/Diff0.png", neofile )
-            counter = counter + 1
+        bmpFile = pngFile + ".bmp"
+        if os.path.exists(bmpFile):
+            script_file.write(beyondCompareScriptLine + '"' + pngFile + '" "' + bmpFile + "\"\n")
             ofile.write("""
             <table>
                 <!-- <tr><td>{0}</td><td>{1}</td><td>{2}</td></tr> -->
                 <tr>
-                    <td><img width="64" height="64" src="file:///{0}" /></td>
+                    <td><img width="64" height="64" src="{0}" /></td>
                     <td><img width="64" height="64" src="{1}" /></td>
-                    <td><img width="64" height="64" src="file:///{2}" /></td>
+                    <td><img width="64" height="64" src="{3}" /></td>
+                    <td><img width="64" height="64" src="{2}" /></td>
                     <td>{0}</td></tr>
             </table>
-            """.format(left, neofile, right))
+            """.format(pngFile, "BcImages/Diff" + str(counter) + ".png", bmpFile,
+                        "BcImages/DiffMono" + str(counter) + ".png" ))
+            counter = counter + 1
         
+    script_file.close()
     ofile.write("</body></html>")
     ofile.close()
+    subprocess.call(["C:\Program Files (x86)\Beyond Compare 3\BComp.com", 
+                                    "/closescript", "@test_script.txt"])
+
+def convertImages():
+    for file in glob.glob("dist/build/imageTest/imageTest*"):
+        if os.path.isfile(file):
+            subprocess.call([file])
 
 if __name__ == "__main__":
+    convertImages()
     performTests()
 
