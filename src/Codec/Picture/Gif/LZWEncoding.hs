e29d57436d0803cee8ea7b88ff162b234dd2858e
Vincent Berthoux <twinside@gmail.com>
2014-01-11 11:31:21 +0100

    First corect LZW encoding :-)

--- src/Codec/Picture/Gif/LZWEncoding.hs
+++ src/Codec/Picture/Gif/LZWEncoding.hs
@@ -12,8 +12,8 @@ import qualified Data.Vector.Storable as V
 
 import Codec.Picture.BitWriter
 
-import Debug.Trace
-import Text.Printf
+{-import Debug.Trace-}
+{-import Text.Printf-}
 
 type Trie = I.IntMap TrieNode
 
@@ -40,38 +40,38 @@ lookupUpdate vector freeIndex firstIndex trie =
         (lzwOutputIndex, nextReadIndex, fromMaybe trie sub)
 
     maxi = V.length vector
-    go !prev !prevIndex !index
+    go !currentTrie !prevIndex !index
       | index >= maxi = (prevIndex, index, Nothing)
-      | otherwise = case I.lookup val prev of
+      | otherwise = case I.lookup val currentTrie of
           Just (TrieNode ix subTable) ->
-              let (lzwOutputIndex, nextReadIndex, subTable') =
-                        go subTable ix $ index + 1 in
-              (lzwOutputIndex, nextReadIndex, updateNode val . TrieNode ix <$> subTable')
-          Nothing | index == maxi -> (prevIndex, index, Nothing)
-                  | otherwise ->
-                    (prevIndex, index, Just $ updateNode nextVal newNode)
-      where val = fromIntegral $ vector `V.unsafeIndex` index
-            nextVal = fromIntegral $ vector `V.unsafeIndex` (index + 1)
-            newNode = emptyNode { trieIndex = freeIndex }
-            updateNode at node = I.insert at node prev 
+              let (lzwOutputIndex, nextReadIndex, newTable) =
+                        go subTable ix $ index + 1
+                  tableUpdater t =
+                      I.insert val (TrieNode ix t) currentTrie
+              in
+              (lzwOutputIndex, nextReadIndex, tableUpdater <$> newTable)
 
--- Untested? untested.
+          Nothing | index == maxi -> (prevIndex, index, Nothing)
+                  | otherwise -> (prevIndex, index, Just $ I.insert val newNode currentTrie)
+
+      where val = fromIntegral $ vector `V.unsafeIndex` index
+            newNode = emptyNode { trieIndex = freeIndex }
+
 lzwEncode :: V.Vector Word8 -> L.ByteString
 lzwEncode vec = runST $ do
     bitWriter <- newWriteStateRef 
 
-    let go _ _ readIndex _ | readIndex >= maxi = return ()
-        go codeSize writeIndex readIndex trie = do
+    let go (codeSize, _) readIndex _ | readIndex >= maxi =
+            writeBitsGif bitWriter (fromIntegral endOfInfo) codeSize
+        go (codeSize, writeIndex) readIndex trie = do
             let (indexToWrite, endIndex, trie') =
                     lookuper writeIndex readIndex trie
-                reversedBits = trace (printf "(%2d) idx:%d" codeSize indexToWrite) 
-                             $ fromIntegral indexToWrite
-            writeBitsGif bitWriter reversedBits codeSize
+            writeBitsGif bitWriter (fromIntegral indexToWrite) codeSize
             go (updateCodeSize codeSize writeIndex)
-               (writeIndex + 1) endIndex trie'
+               endIndex trie'
 
     writeBitsGif bitWriter (fromIntegral clearCode) startCodeSize
-    go startCodeSize firstFreeIndex 0 initialTrie
+    go (startCodeSize, firstFreeIndex) 0 initialTrie
 
     finalizeBoolWriter bitWriter
   where
@@ -86,7 +86,8 @@ lzwEncode vec = runST $ do
     
     lookuper = lookupUpdate vec
     updateCodeSize codeSize writeIdx
-        | writeIdx == 2 ^ codeSize = min 12 $ codeSize + 1
-        | otherwise = codeSize
+        | writeIdx == 2 ^ codeSize =
+                (min 12 $ codeSize + 1, writeIdx + 1)
+        | otherwise = (codeSize, writeIdx + 1)
 
 
