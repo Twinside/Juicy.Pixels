71a23bcaf8e8fd8a84d53bd8015ef592a9c55666
Vincent Berthoux <twinside@gmail.com>
2014-01-17 23:51:45 +0100

    Adding an helper function to import image from ForeignPtr

--- src/Codec/Picture/VectorByteConversion.hs
+++ src/Codec/Picture/VectorByteConversion.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Codec.Picture.VectorByteConversion( blitVector, toByteString  ) where
+{-# LANGUAGE TypeFamilies #-}
+module Codec.Picture.VectorByteConversion( blitVector
+                                         , toByteString
+                                         , imageFromUnsafePtr ) where
 
 import Data.Word( Word8 )
-import Data.Vector.Storable( Vector, unsafeToForeignPtr )
-import Foreign.ForeignPtr.Safe( castForeignPtr )
+import Data.Vector.Storable( Vector, unsafeToForeignPtr, unsafeFromForeignPtr0 )
+import Foreign.ForeignPtr.Safe( ForeignPtr, castForeignPtr )
 import Foreign.Storable( Storable, sizeOf )
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as S
+
+import Codec.Picture.Types
 
 blitVector :: Vector Word8 -> Int -> Int -> B.ByteString
 blitVector vec atIndex blitSize = S.PS ptr (offset + atIndex) blitSize
@@ -17,4 +22,12 @@ toByteString :: forall a. (Storable a) => Vector a -> B.ByteString
 toByteString vec = S.PS (castForeignPtr ptr) offset (len * size)
   where (ptr, offset, len) = unsafeToForeignPtr vec
         size = sizeOf (undefined :: a)
+
+imageFromUnsafePtr :: forall px
+                    . (Pixel px, (PixelBaseComponent px) ~ Word8)
+                   => Int -> Int -> ForeignPtr Word8 -> Image px
+imageFromUnsafePtr width height ptr =
+    Image width height $ unsafeFromForeignPtr0 ptr size
+      where compCount = componentCount (undefined :: px)
+            size = width * height * compCount
 
