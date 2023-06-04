b3f1c16d4e77393198938294e952aa14b4f9e9b9
Bodigrim <andrew.lelechenko@gmail.com>
2023-06-04 19:13:57 +0100

    Future-proof against potential Prelude.foldl'

--- src/Codec/Picture/Metadata.hs
+++ src/Codec/Picture/Metadata.hs
@@ -43,6 +43,8 @@ module Codec.Picture.Metadata( -- * Types
                              , dotsPerCentiMeterToDotPerInch
                              ) where
 
+import Prelude hiding (Foldable(..))
+
 #if !MIN_VERSION_base(4,8,0)
 import Data.Monoid( Monoid, mempty, mappend )
 import Data.Word( Word )
